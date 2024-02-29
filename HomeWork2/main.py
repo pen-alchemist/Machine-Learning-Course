03c9eb52a94979bd262f0cccba3c58b1c83a1117
Egor Romanov <alchemy.follower666@gmail.com>
2024-02-29 06:18:44 +0300

    HomeWork2 Done.

--- HomeWork2/main.py
+++ HomeWork2/main.py
@@ -1,62 +1,63 @@
 import pickle
+from pathlib import Path
+
 import numpy as np
 import pandas as pd
 
 from joblib import dump, load
 from matplotlib import pyplot as plt
 
-from sklearn import svm
-from sklearn import datasets
-from sklearn.metrics import det_curve
 from sklearn.preprocessing import normalize
 from sklearn.datasets import make_regression
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
 
 if __name__ == "__main__":
+    file_path = Path(__file__).resolve().parent
+
     # Read csv file with pandas
-    csv_path = '../kc_house_data.csv'
+    csv_path = f'{file_path}/kc_house_data.csv'
     df = pd.read_csv(csv_path)
 
     # Make basic data analysis with pandas
     df.head()
-    print(df.shape)
-    print(df.columns)
-    print(df.info())
-    print(df.describe())
-    print(df.sort_values(by='sqft_living', ascending=True))
-    print(df['sqft_living'].mean())
-    print(df['sqft_living'].min())
-    print(df['sqft_living'].max())
-    print(df['sqft_living'].skew())
+    print(f'df.shape\n {df.shape}')
+    print(f'df.columns\n {df.columns}')
+    print(f'df.info()\n {df.info()}')
+    print(f'df.describe()\n {df.describe()}')
+    print(f'df.sort_values\n {df.sort_values(by="sqft_living", ascending=True)}')
+    print(f'df[\'sqft_living\'].mean()\n {df["sqft_living"].mean()}')
+    print(f'df[\'sqft_living\'].min()\n {df["sqft_living"].min()}')
+    print(f'df[\'sqft_living\'].max()\n {df["sqft_living"].max()}')
+    print(f'df[\'sqft_living\'].skew()\n {df["sqft_living"].skew()}')
     comp_df = df[df['sqft_living'] > 10000]
-    print(comp_df)
-    print(df[['yr_built', 'yr_renovated']].median())
+    print(f'Comparation df "sqft_living" with 10000\n {comp_df}')
+    print(f'df[[\'yr_built\', \'yr_renovated\']].median()\n '
+          f'{df[["yr_built", "yr_renovated"]].median()}')
 
     # Select columns from dataset that can be used in multivariate linear regression
     multi_selection = df.drop(['id', 'date', 'price', 'zipcode'], axis=1)
-    print(multi_selection.head(101))
+    print(f'multi_selection.head(101)\n {multi_selection.head(101)}')
     X = np.asarray(df['sqft_living'].values.tolist())
     # Reshaping the Dependent features
     X = X.reshape(len(X), 1)  # Changing the shape from (50,) to (50,1)\
-    print(X)
-    print(X.shape)
-    print(X.max() - X.min())
+    print(f'X is:\n {X}')
+    print(f'X.shape\n {X.shape}')
+    print(f'X.max() - X.min()\n {X.max() - X.min()}')
     y = np.asarray(df['price'].values.tolist())
     # Reshaping the Dependent features
     y = y.reshape(len(y), 1)  # Changing the shape from (50,) to (50,1)
-    print(y)
-    print(y.shape)
+    print(f'y is:\n {y}')
+    print(f'y.shape\n {y.shape}')
     plt.plot(X, y, color='green', marker='o', linestyle='solid')
     plt.xlabel('sqft_living')
     plt.ylabel('price')
-    plt.savefig('graph1.png')
-    plt.savefig('graph1.pdf')
+    plt.savefig(f'{file_path}/graph1.png')
+    plt.savefig(f'{file_path}/graph1.pdf')
 
     # Let's create a DataFrame "Independent_Variables" to visualize our final independent features
     independ_variables = pd.DataFrame(X)
-    print(independ_variables)
+    print(f'Independent variables:\n {independ_variables}')
 
     # Univariate model training
     random_state = 42
@@ -65,38 +66,39 @@ if __name__ == "__main__":
         X, y, test_size=0.4, random_state=random_state)
 
     model = LinearRegression()
-    print(model)
+    print(f'Model:\n {model}')
     model.fit(X_train, y_train)
-    print(model.fit(X_train, y_train))
+    print(f'model.fit(X_train, y_train)\n {model.fit(X_train, y_train)}')
 
     model.predict(X_test)
-    print(model.predict(X_test))
+    print(f'model.predict(X_test)\n {model.predict(X_test)}')
     model.score(X_test, y_test)
-    print(model.score(X_test, y_test))
+    print(f'model.score(X_test, y_test)\n {model.score(X_test, y_test)}')
 
     # Save model with pickle
-    with open('../modelmultivar.pkl', 'wb') as file:
+    with open(f'{file_path}/modelmultivar.pkl', 'wb') as file:
         pickle.dump(model, file)
 
     # Save model with joblib
-    dump(model, '../modelunivar.joblib')
+    dump(model, f'{file_path}/modelunivar.joblib')
     # Save data to test
-    dump([X_test, y_test], '../test_data_univar.joblib')
+    dump([X_test, y_test], f'{file_path}/test_data_univar.joblib')
 
     # Multivariate model training
     multi_selection = df.drop(['id', 'date', 'price', 'zipcode'], axis=1)
-    print(multi_selection)
+    print(f'multi_selection (X):\n {multi_selection}')
     X = np.asarray(multi_selection.values.tolist())
-    print(X)
-    print(X.shape)
+    print(f'X is:\n {X}')
+    print(f'X.shape\n {X.shape}')
     y = np.asarray(df['price'].values.tolist())
-    print(y)
-    print(y.shape)
+    print(f'y is:\n {y}')
+    print(f'y.shape\n {y.shape}')
     y = y.reshape(len(y), 1)
-    print(y.shape)
+    print(f'y.shape\n {y.shape}')
     X_normalized = normalize(X, norm="l1")
+    print(f'X_normalized\n {X_normalized}')
     print(X_normalized)
-    print(X_normalized.shape)
+    print(f'X_normalized.shape\n {X_normalized.shape}')
 
     random_state = 101
 
@@ -119,11 +121,11 @@ if __name__ == "__main__":
         pickle.dump(model, file)
 
     # Save model with joblib
-    dump(model, '../modelmultivar.joblib')
+    dump(model, f'{file_path}/modelmultivar.joblib')
 
-    loaded_multimodel = load('../modelmultivar.joblib')
-    loaded_unimodel = load('../modelunivar.joblib')
-    loaded_testdata = load('../test_data_univar.joblib')
+    loaded_multimodel = load(f'{file_path}/modelmultivar.joblib')
+    loaded_unimodel = load(f'{file_path}/modelunivar.joblib')
+    loaded_testdata = load(f'{file_path}/test_data_univar.joblib')
 
     loaded_multimodel.predict(X_test)
     multi_predict = loaded_multimodel.predict(X_test)
@@ -131,7 +133,7 @@ if __name__ == "__main__":
     print(multi_predict)
 
     plt.scatter(multi_predict, y_test, color='red', marker='o')
-    plt.savefig('graph2.png')
+    plt.savefig(f'{file_path}/graph2.png')
 
     loaded_unimodel.predict(loaded_testdata[0])
     univar_predict = loaded_unimodel.predict(loaded_testdata[0])
@@ -139,10 +141,10 @@ if __name__ == "__main__":
     print(univar_predict)
 
     plt.scatter(univar_predict, loaded_testdata[1], color='green', marker='o')
-    plt.savefig('graph3.png')
+    plt.savefig(f'{file_path}/graph3.png')
 
     # Creating two multiple linear regression models:
-    csv_path = '../customers-100.csv'
+    csv_path = f'{file_path}/customers-100.csv'
     df = pd.read_csv(csv_path)
 
     df.head()
@@ -176,13 +178,13 @@ if __name__ == "__main__":
     print(prediction)
 
     plt.scatter(prediction, y_test, color='yellow', marker='o')
-    plt.savefig('graph4.png')
+    plt.savefig(f'{file_path}/graph4.png')
 
     model.score(X_test, y_test)
     print(model.score(X_test, y_test))
     model.coef_
     print(model.coef_)
-    dump(model, '../multivarmodel1.joblib')
+    dump(model, f'{file_path}/multivarmodel1.joblib')
 
     y = np.asarray(df['Vacation'].values.tolist())
     y = y.reshape(len(y), 1)
@@ -199,16 +201,16 @@ if __name__ == "__main__":
     print(prediction)
 
     plt.scatter(prediction, y_test, color='blue', marker='o')
-    plt.savefig('graph5.png')
+    plt.savefig(f'{file_path}/graph5.png')
 
     model.score(X_test, y_test)
     print(model.score(X_test, y_test))
     model.coef_
     print(model.coef_)
-    dump(model, '../multivarmodel2.joblib')
+    dump(model, f'{file_path}/multivarmodel2.joblib')
 
-    model1 = load('../multivarmodel1.joblib').predict(X_test)
-    model2 = load('../multivarmodel2.joblib').predict(X_test)
+    model1 = load(f'{file_path}/multivarmodel1.joblib').predict(X_test)
+    model2 = load(f'{file_path}/multivarmodel2.joblib').predict(X_test)
 
     plt.plot(model1, model2, color='blue', marker='o')
-    plt.savefig('graph6.png')
+    plt.savefig(f'{file_path}/graph6.png')
