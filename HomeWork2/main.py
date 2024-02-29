33e846021d45ffd28532fbd4b6ce79e43e574a44
Egor Romanov <alchemy.follower666@gmail.com>
2024-02-29 06:36:41 +0300

    HomeWork2 Done.

--- HomeWork2/main.py
+++ HomeWork2/main.py
@@ -107,14 +107,14 @@ if __name__ == "__main__":
         X_normalized, y, test_size=0.3, random_state=random_state)
 
     model = LinearRegression()
-    print(model)
+    print(f'Model:\n {model}')
     model.fit(X_train, y_train)
-    print(model.fit(X_train, y_train))
+    print(f'model.fit(X_train, y_train)\n {model.fit(X_train, y_train)}')
 
     model.predict(X_test)
     print(model.predict(X_test))
     model.score(X_test, y_test)
-    print(model.score(X_test, y_test))
+    print(f'model.score(X_test, y_test)\n {model.score(X_test, y_test)}')
 
     # Save model with pickle
     with open('../modelmultivar.pkl', 'wb') as file:
@@ -130,7 +130,7 @@ if __name__ == "__main__":
     loaded_multimodel.predict(X_test)
     multi_predict = loaded_multimodel.predict(X_test)
 
-    print(multi_predict)
+    print(f'multivar_predict:\n {multi_predict}')
 
     plt.scatter(multi_predict, y_test, color='red', marker='o')
     plt.savefig(f'{file_path}/graph2.png')
@@ -138,7 +138,7 @@ if __name__ == "__main__":
     loaded_unimodel.predict(loaded_testdata[0])
     univar_predict = loaded_unimodel.predict(loaded_testdata[0])
 
-    print(univar_predict)
+    print(f'univar_predict:\n {univar_predict}')
 
     plt.scatter(univar_predict, loaded_testdata[1], color='green', marker='o')
     plt.savefig(f'{file_path}/graph3.png')
@@ -149,19 +149,26 @@ if __name__ == "__main__":
 
     df.head()
     multi_selection = df.drop(
-        ['Salary', 'Vacation',
-         'Index', 'Customer Id',
-         'First Name', 'Last Name',
-         'Phone 1', 'Phone 2'],
+        [
+            'Salary',
+            'Vacation',
+            'Index',
+            'Customer Id',
+            'First Name',
+            'Last Name',
+            'Phone 1',
+            'Phone 2',
+            'Email'
+        ],
         axis=1
     )
     X = np.asarray(multi_selection.values.tolist())
-    print(X)
-    print(X.shape)
+    print(f'X is:\n {X}')
+    print(f'X.shape\n {X.shape}')
     y = np.asarray(df['Salary'].values.tolist())
     y = y.reshape(len(y), 1)
-    print(y)
-    print(y.shape)
+    print(f'y is:\n {y}')
+    print(f'y.shape\n {y.shape}')
 
     random_state = 101
     X, y = make_regression(random_state=random_state, n_features=17, noise=17)
@@ -169,27 +176,27 @@ if __name__ == "__main__":
         X, y, test_size=0.3, random_state=random_state)
 
     model = LinearRegression()
-    print(model)
+    print(f'Model:\n {model}')
     model.fit(X_train, y_train)
-    print(model.fit(X_train, y_train))
+    print(f'model.fit(X_train, y_train)\n {model.fit(X_train, y_train)}')
 
     model.predict(X_test)
     prediction = model.predict(X_test)
-    print(prediction)
+    print(f'Prediction:\n {prediction}')
 
     plt.scatter(prediction, y_test, color='yellow', marker='o')
     plt.savefig(f'{file_path}/graph4.png')
 
     model.score(X_test, y_test)
-    print(model.score(X_test, y_test))
+    print(f'model.score(X_test, y_test)\n {model.score(X_test, y_test)}')
     model.coef_
-    print(model.coef_)
+    print(f'model.coef_:\n {model.coef_}')
     dump(model, f'{file_path}/multivarmodel1.joblib')
 
     y = np.asarray(df['Vacation'].values.tolist())
     y = y.reshape(len(y), 1)
-    print(y)
-    print(y.shape)
+    print(f'y is:\n {y}')
+    print(f'y.shape\n {y.shape}')
 
     random_state = 101
     X, y = make_regression(random_state=random_state, n_features=17, noise=17)
@@ -198,15 +205,15 @@ if __name__ == "__main__":
 
     model.predict(X_test)
     prediction = model.predict(X_test)
-    print(prediction)
+    print(f'Prediction:\n {prediction}')
 
     plt.scatter(prediction, y_test, color='blue', marker='o')
     plt.savefig(f'{file_path}/graph5.png')
 
     model.score(X_test, y_test)
-    print(model.score(X_test, y_test))
+    print(f'model.score(X_test, y_test)\n {model.score(X_test, y_test)}')
     model.coef_
-    print(model.coef_)
+    print(f'model.coef_:\n {model.coef_}')
     dump(model, f'{file_path}/multivarmodel2.joblib')
 
     model1 = load(f'{file_path}/multivarmodel1.joblib').predict(X_test)
