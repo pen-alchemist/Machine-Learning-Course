20352a050714d5c7dbac759cf995a3a5d541afbe
Egor Romanov <alchemy.follower666@gmail.com>
2024-04-18 18:46:45 +0300

    HomeWork11 Done.

--- HomeWork11/main.py
+++ HomeWork11/main.py
@@ -1,26 +1,15 @@
-import pandas as pd
-import numpy as np
 import re
-import string
 import nltk
-import gensim
+import string
 
-from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.stem import SnowballStemmer
-from nltk.corpus import wordnet
 from nltk import word_tokenize
 
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import classification_report, f1_score, accuracy_score, confusion_matrix
-from sklearn.metrics import roc_curve, auc, roc_auc_score# bag of words
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import CountVectorizer#for word embedding
+from keras.datasets import imdb
 
-from gensim.models import Word2Vec
+from sklearn.feature_extraction.text import CountVectorizer
+
+from gensim.models import KeyedVectors
 
 
 nltk.download('punkt')
